da5aa5c9e3e73db4b9bd04c8f2acb1121bf07b2b
jaideepsarkar19 <113533055+jaideepsarkar19@users.noreply.github.com>
2023-05-18 10:51:28 +0530

    2file modified

--- _helpers/fake-backend.js
+++ _helpers/fake-backend.js
@@ -1,120 +1,128 @@
-import { Role } from './'
+import { Role } from './';
 
 export function configureFakeBackend() {
-    // array in local storage for user records
-    let users = JSON.parse(localStorage.getItem('users')) || [{ 
-        id: 1,
-        title: 'Mr',
-        firstName: 'Joe',
-        lastName: 'Bloggs',
-        email: 'joe@bloggs.com',
-        role: Role.User,
-        password: 'joe123'
-    }];
+  // array in local storage for user records
+  let users = JSON.parse(localStorage.getItem('users')) || [
+    {
+      id: 1,
+      title: 'Mr',
+      firstName: 'Jaideep',
+      lastName: 'Sarkar',
+      email: 'helpjs@19gmail.com.com',
+      role: Role.User,
+      password: '123pwd',
+    },
+  ];
 
-    // monkey patch fetch to setup fake backend
-    let realFetch = window.fetch;
-    window.fetch = function (url, opts) {
-        return new Promise((resolve, reject) => {
-            // wrap in timeout to simulate server api call
-            setTimeout(handleRoute, 500);
+  // monkey patch fetch to setup fake backend
+  let realFetch = window.fetch;
+  window.fetch = function (url, opts) {
+    return new Promise((resolve, reject) => {
+      // wrap in timeout to simulate server api call
+      setTimeout(handleRoute, 500);
 
-            function handleRoute() {
-                const { method } = opts;
-                switch (true) {
-                    case url.endsWith('/users') && method === 'GET':
-                        return getUsers();
-                    case url.match(/\/users\/\d+$/) && method === 'GET':
-                        return getUserById();
-                    case url.endsWith('/users') && method === 'POST':
-                        return createUser();
-                    case url.match(/\/users\/\d+$/) && method === 'PUT':
-                        return updateUser();
-                    case url.match(/\/users\/\d+$/) && method === 'DELETE':
-                        return deleteUser();
-                    default:
-                        // pass through any requests not handled above
-                        return realFetch(url, opts)
-                            .then(response => resolve(response))
-                            .catch(error => reject(error));
-                }
-            }
+      function handleRoute() {
+        const { method } = opts;
+        switch (true) {
+          case url.endsWith('/users') && method === 'GET':
+            return getUsers();
+          case url.match(/\/users\/\d+$/) && method === 'GET':
+            return getUserById();
+          case url.endsWith('/users') && method === 'POST':
+            return createUser();
+          case url.match(/\/users\/\d+$/) && method === 'PUT':
+            return updateUser();
+          case url.match(/\/users\/\d+$/) && method === 'DELETE':
+            return deleteUser();
+          default:
+            // pass through any requests not handled above
+            return realFetch(url, opts)
+              .then((response) => resolve(response))
+              .catch((error) => reject(error));
+        }
+      }
 
-            // route functions
+      // route functions
 
-            function getUsers() {
-                return ok(users);
-            }
+      function getUsers() {
+        return ok(users);
+      }
 
-            function getUserById() {
-                let user = users.find(x => x.id === idFromUrl());
-                return ok(user);
-            }
-    
-            function createUser() {
-                const user = body();
+      function getUserById() {
+        let user = users.find((x) => x.id === idFromUrl());
+        return ok(user);
+      }
 
-                if (users.find(x => x.email === user.email)) {
-                    return error(`User with the email ${user.email} already exists`);
-                }
+      function createUser() {
+        const user = body();
 
-                // assign user id and a few other properties then save
-                user.id = newUserId();
-                user.dateCreated = new Date().toISOString();
-                delete user.confirmPassword;
-                users.push(user);
-                localStorage.setItem('users', JSON.stringify(users));
+        if (users.find((x) => x.email === user.email)) {
+          return error(`User with the email ${user.email} already exists`);
+        }
 
-                return ok();
-            }
-    
-            function updateUser() {
-                let params = body();
-                let user = users.find(x => x.id === idFromUrl());
+        // assign user id and a few other properties then save
+        user.id = newUserId();
+        user.dateCreated = new Date().toISOString();
+        delete user.confirmPassword;
+        users.push(user);
+        localStorage.setItem('users', JSON.stringify(users));
 
-                // only update password if included
-                if (!params.password) {
-                    delete params.password;
-                }
-                // don't save confirm password
-                delete params.confirmPassword;
+        return ok();
+      }
 
-                // update and save user
-                Object.assign(user, params);
-                localStorage.setItem('users', JSON.stringify(users));
+      function updateUser() {
+        let params = body();
+        let user = users.find((x) => x.id === idFromUrl());
 
-                return ok();
-            }
-    
-            function deleteUser() {
-                users = users.filter(x => x.id !== idFromUrl());
-                localStorage.setItem('users', JSON.stringify(users));
+        // only update password if included
+        if (!params.password) {
+          delete params.password;
+        }
+        // don't save confirm password
+        delete params.confirmPassword;
 
-                return ok();
-            }
-    
-            // helper functions
+        // update and save user
+        Object.assign(user, params);
+        localStorage.setItem('users', JSON.stringify(users));
 
-            function ok(body) {
-                resolve({ ok: true, text: () => Promise.resolve(JSON.stringify(body)) });
-            }
+        return ok();
+      }
 
-            function error(message) {
-                resolve({ status: 400, text: () => Promise.resolve(JSON.stringify({ message })) });
-            }
+      function deleteUser() {
+        users = users.filter((x) => x.id !== idFromUrl());
+        localStorage.setItem('users', JSON.stringify(users));
 
-            function idFromUrl() {
-                const urlParts = url.split('/');
-                return parseInt(urlParts[urlParts.length - 1]);
-            }
+        return ok();
+      }
 
-            function body() {
-                return opts.body && JSON.parse(opts.body);    
-            }
+      // helper functions
 
-            function newUserId() {
-                return users.length ? Math.max(...users.map(x => x.id)) + 1 : 1;
-            }
+      function ok(body) {
+        resolve({
+          ok: true,
+          text: () => Promise.resolve(JSON.stringify(body)),
         });
-    }
+      }
+
+      function error(message) {
+        resolve({
+          status: 400,
+          text: () => Promise.resolve(JSON.stringify({ message })),
+        });
+      }
+
+      function idFromUrl() {
+        const urlParts = url.split('/');
+        return parseInt(urlParts[urlParts.length - 1]);
+      }
+
+      function body() {
+        return opts.body && JSON.parse(opts.body);
+      }
+
+      function newUserId() {
+        return users.length ? Math.max(...users.map((x) => x.id)) + 1 : 1;
+      }
+    });
+  };
 }
